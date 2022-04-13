614cfb5ac36011f84f626fc52f00be1d9ca22889
CyberChoom <87709357+CyberChoom@users.noreply.github.com>
2022-04-12 23:56:33 -0400

    Initial commit

--- Assignment 2.2.py
+++ Assignment 2.2.py
@@ -1,14 +1,14 @@
 print("Exercise #1")
-my_string="Test Automation"
-word_test=my_string[:4]
+my_string = "Test Automation"
+word_test = my_string[:4]
 print(word_test)
 
 print("Exercise #2")
-my_string="Test Automation"
-second_word=my_string[5:]
+my_string = "Test Automation"
+second_word = my_string[5:]
 print(second_word)
 
 print("Exercise #3")
-my_list=[10,50,20,25,30]
-sliced_list=my_list[1:3]
+my_list = [10, 50, 20, 25, 30]
+sliced_list = my_list[1:3]
 print(sliced_list)
