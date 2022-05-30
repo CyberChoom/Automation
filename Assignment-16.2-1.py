ebd137e03323ad034dd99eebe93b47cb3555d3e3
CyberChoom <87709357+CyberChoom@users.noreply.github.com>
2022-05-29 22:51:45 -0400

    Initial commit

---
+++ Assignment-16.2-1.py
@@ -0,0 +1,8 @@
+def show_employee(name, salary=50000):
+    print("Name:", name)
+    print("Salary:", salary)
+
+
+show_employee(name="Ron", salary=12)
+show_employee("Jack", 200)
+show_employee("Bob")
