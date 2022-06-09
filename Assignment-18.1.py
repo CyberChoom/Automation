5cb894babdf3c502a3038a2d1ed6c3de86f9ea4e
CyberChoom <87709357+CyberChoom@users.noreply.github.com>
2022-06-09 09:39:18 -0400

    Initial commit

---
+++ Assignment-18.1.py
@@ -0,0 +1,22 @@
+import random
+
+random_number = random.randint(0, 50)
+print(random_number)
+print("Guess the number between 0 and 50. You will have 7 tries.")
+
+
+def game():
+    for item in range(7):
+        user_input = int(input("Enter the number below:\n"))
+        if user_input < random_number:
+            print("The value is too low")
+        elif user_input > random_number:
+            print("The value is too high")
+        elif user_input == random_number:
+            print("You guessed correctly")
+            return
+    print("You were not able to guess the number."
+          "\nRestart the game to continue, the number will be reset.")
+
+
+game()
